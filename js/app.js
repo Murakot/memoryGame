fb4023bebcaca036acb6c46ad7714cafb0acd32e
Murakot <FNB9ihJtu8bc7F2>
2019-03-16 12:53:34 +0100

    feat: Cards are flipping

--- js/app.js
+++ js/app.js
@@ -36,3 +36,12 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+const allCards = document.querySelectorAll('.card');
+
+allCards.forEach(function(card) {
+	card.addEventListener('click', function(c){
+		card.classList.add('open','show');
+		console.log(c);	
+	})
+});
